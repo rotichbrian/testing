93883e10ffa580fc078b226dd317750ab34b5adf
Brian Kipkorir <rotichbrian891@gmail.com>
2024-04-09 17:32:43 +0300

    Assignment

--- index.js
+++ index.js
@@ -56,4 +56,22 @@ const nestedMuppet = {
 };
 const {song2, song4} = {...nestedMuppet.album.theMuppetMovie}
 const {nestedJob, nestedPartner} = nestedMuppet
+const {muppetName, color, song, job, partner} = muppet
+
+const nestedMuppet = {
+  nestedName: 'Kermit',
+  nestedColor: 'green',
+  album: {
+    theMuppetMovie: {
+      song1: 'Rainbow Connection',
+      song2: 'Moving Right Along',
+      song3: 'Never Before, Never Again',
+      song4: 'I Hope That Something Better Comes Along',
+    },
+  },
+  nestedJob: 'Host of The Muppet Show',
+  nestedPartner: 'Miss Piggy'
+};
+const {song2, song4} = {...nestedMuppet.album.theMuppetMovie}
+const {nestedJob, nestedPartner} = nestedMuppet
 
